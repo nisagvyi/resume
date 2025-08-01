16afd568ce8afdb2e48ea7934f8c496ad7d155fb
nisagvyi <guveyinisa@gmail.com>
2025-08-01 20:41:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// (html, css)görüntü tasarım animasyon
+// (js) fonksiyonel
+
+function onResumeClick() {
+ var element = document.getElementById('resume') 
+ element.scrollIntoView()
+}
+function onAboutClick (){
+    var element = document.getElementById('about')
+    element.scrollIntoView()
+}
+function onCVClick (){
+   var elemnt = document.getElementById('CV') 
+   elemnt.scrollIntoView()
+}
+function onLinkednClick(){
+    window.open('https://www.linkedin.com/in/nisa-nur-g%C3%BCveyi-69a80222a/')
+}
+function onKariyerClick(){
+    window.open('https://www.kariyer.net/ozgecmis/nisanurguveyi')
+}
+ 
